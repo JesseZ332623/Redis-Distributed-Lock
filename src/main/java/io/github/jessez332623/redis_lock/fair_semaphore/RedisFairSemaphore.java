0a9e43f5c430a57872d1455d7186b7cc2c2c8a20
Jesse <3191955858@qq.com>
2025-09-20 20:02:47 +0800

    Redis 公平信号量接口。

---
+++ src/main/java/io/github/jessez332623/redis_lock/fair_semaphore/RedisFairSemaphore.java
@@ -0,0 +1,31 @@
+package io.github.jessez332623.redis_lock.fair_semaphore;
+
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
+/** Redis 公平信号量接口。*/
+public interface RedisFairSemaphore
+{
+    /**
+     * 兼容响应式流的 Redis 公平信号量操作，
+     * 使用 {@link Mono#usingWhen(Publisher, Function, Function)} 方法，
+     * 在业务逻辑（action）范围前后，自动完成信号量的获取与释放操作。
+     *
+     * @param <T> 在信号量作用域中业务逻辑返回的类型
+     *
+     * @param semaphoreName 信号量键名（例 semaphore:remote）
+     * @param limit         最大信号量值
+     * @param timeout       信号量有效期（单位：秒）
+     * @param action        业务逻辑
+     *
+     * @return 发布业务逻辑执行结果数据的 {@link Mono}
+     */
+    <T> Mono<T>
+    withFairSemaphore(
+        String semaphoreName,
+        long limit, long timeout,
+        Function<String, Mono<T>> action
+    );
+}
