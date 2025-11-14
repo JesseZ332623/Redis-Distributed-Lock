0be653e1cb7265f589234b814cfe85a9af681c26
浩进 朱 <3191955858@qq.com>
2025-11-14 09:38:33 +0800

    当等待获取 Redis 锁超过设定的时间限制，抛本异常。

--- src/main/java/io/github/jessez332623/redis_lock/distributed_lock/exception/AcquireLockTimeout.java
+++ src/main/java/io/github/jessez332623/redis_lock/distributed_lock/exception/AcquireLockTimeout.java
@@ -1,12 +1,12 @@
 package io.github.jessez332623.redis_lock.distributed_lock.exception;
 
 /** 当等待获取 Redis 锁超过设定的时间限制，抛本异常。*/
-public class RedisDistributedLockAcquireTimeout extends RuntimeException
+public class AcquireLockTimeout extends RuntimeException
 {
-    public RedisDistributedLockAcquireTimeout(String message) {
+    public AcquireLockTimeout(String message) {
         super(message);
     }
-    public RedisDistributedLockAcquireTimeout(String message, Throwable throwable) {
+    public AcquireLockTimeout(String message, Throwable throwable) {
         super(message, throwable);
     }
 }
