e91b745d680257ec915be9249f8cae1b39c37281
浩进 朱 <3191955858@qq.com>
2025-11-14 09:41:39 +0800

    Redis 分布式锁接口类。

--- src/main/java/io/github/jessez332623/redis_lock/distributed_lock/RedisDistributedLock.java
+++ src/main/java/io/github/jessez332623/redis_lock/distributed_lock/RedisDistributedLock.java
@@ -1,5 +1,6 @@
 package io.github.jessez332623.redis_lock.distributed_lock;
 
+import io.github.jessez332623.redis_lock.statistics.StatisticalInstrument;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Mono;
 
@@ -7,7 +8,7 @@ import java.time.Duration;
 import java.util.function.Function;
 
 /** Redis 分布式锁接口类。*/
-public interface RedisDistributedLock
+public interface RedisDistributedLock extends StatisticalInstrument
 {
     /**
      * 兼容响应式流的 Redis 分布式锁操作，
